57be8057b0f6647048b6717ab106f18993390e39
sruthisagar <sruthidingu@gmail.com>
2020-07-25 00:13:55 +0530

    Version 5

--- SruPoke.py
+++ SruPoke.py
@@ -2,6 +2,7 @@
 # The dots bounce off whenever they touch the edge of the window.
 # The dots teleport to random positions on each mouse click.
 # Scoreboard is displayed.
+# GAME OVER is displayed once the dots collide.
 
 from uagame import Window
 from pygame import QUIT, Color, MOUSEBUTTONUP
@@ -9,6 +10,7 @@ from random import randint
 from pygame.time import Clock, get_ticks
 from pygame.event import get as get_events
 from pygame.draw import circle as draw_circle
+from math import sqrt
 
 def main():
 
@@ -29,6 +31,7 @@ class Game:
 		self.small_dot.randomize()
 		self.big_dot.randomize()
 		self.score=0
+		self.continue_game=True
 
 	def adjust_window(self):
 
@@ -43,6 +46,8 @@ class Game:
 		self.draw_score()
 		self.small_dot.draw()
 		self.big_dot.draw()
+		if not self.continue_game:
+			self.draw_game_over()
 		self.window.update()
 
 	def play(self):
@@ -60,7 +65,7 @@ class Game:
 		for event in event_list:
 			if event.type==QUIT:
 				self.close_selected=True
-			elif event.type==MOUSEBUTTONUP:
+			elif self.continue_game and event.type==MOUSEBUTTONUP:
 				self.handle_mouse_click()
 
 	def handle_mouse_click(self):
@@ -75,15 +80,36 @@ class Game:
 
 	def update(self):
 
-		self.small_dot.move()
-		self.big_dot.move()
-		self.clock.tick(self.frame_rate)
-		self.score=get_ticks() // 1000 #milliseconds to seconds
+		if self.continue_game:
+			self.small_dot.move()
+			self.big_dot.move()
+			self.clock.tick(self.frame_rate)
+			self.score=get_ticks() // 1000 #milliseconds to seconds
 
+
+		if self.small_dot.intersects(self.big_dot):
+			self.continue_game=False
+
+	def draw_game_over(self):
+
+		string='GAME OVER'
+		font_color=self.small_dot.get_color()
+		bg_color=self.big_dot.get_color()
+		original_font_color=self.window.get_font_color()
+		original_bg_color=self.window.get_bg_color()
+
+		self.window.set_font_color(font_color)
+		self.window.set_bg_color(bg_color)
+		height=self.window.get_height() - self.window.get_font_height()
+		self.window.draw_string(string, 0, height)
+
+		self.window.set_font_color(original_font_color)
+		self.window.set_bg_color(original_bg_color)
 
 class Dot:
 
 	def __init__(self, window, color, center, radius, velocity):
+
 		self.window=window
 		self.color=color
 		self.center=center
@@ -91,6 +117,7 @@ class Dot:
 		self.velocity=velocity
 
 	def draw(self):
+
 		surface=self.window.get_surface()
 		color=Color(self.color)
 		draw_circle(surface, color, self.center, self.radius)
@@ -108,5 +135,14 @@ class Dot:
 		size=[self.window.get_width(), self.window.get_height()]
 		for index in range(0,2):
 			self.center[index]=randint(self.radius, size[index]-self.radius)
+
+	def intersects(self, dot):
+
+		distance = sqrt((self.center[0] - dot.center[0])**2 + (self.center[1] - dot.center[1])**2)
+		return distance <= self.radius + dot.radius
+
+	def get_color(self):
+
+		return self.color
 
 main()
